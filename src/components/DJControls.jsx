1ede54bdeba069295cd60ec0f82a02b0f52232a9
Charlotte <solce001@mymail.unisa.edu.au>
2025-11-04 14:02:35 +1030

    Add React functions, hook up play and textarea components, and comment out JavaScript

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -6,25 +6,25 @@ function DJControls() {
                 <input type="text" className="form-control" id="cpm_text_input"placeholder="120" aria-label="cpm" aria-describedby="cdpm_label"/>
             </div>
 
-            <label for="volume_range" className="form-label">Volume</label>
+            <label htmlFor="volume_range" className="form-label">Volume</label>
             <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"/>
 
             <div>
                 <div className="form-check">
                     <input className="form-check-input" type="checkbox" value="" id="s1"/>
-                    <label className="form-check-label" for="s1">
+                    <label className="form-check-label" htmlFor="s1">
                     s1
                     </label>
                 </div>
                 <div className="form-check">
                     <input className="form-check-input" type="checkbox" value="" id="d1"/>
-                    <label className="form-check-label" for="d1">
+                    <label className="form-check-label" htmlFor="d1">
                     d1
                     </label>
                 </div>
                 <div className="form-check">
                     <input className="form-check-input" type="checkbox" value="" id="d2"/>
-                    <label className="form-check-label" for="d2">
+                    <label className="form-check-label" htmlFor="d2">
                     d2
                     </label>
                 </div>
